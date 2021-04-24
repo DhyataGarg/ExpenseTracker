bad973c8bb991e33bfcb622713b3b623ec0a7fee
Dhyata Garg <dhyatagarg@gmail.com>
2021-04-24 15:17:23 +0530

    Add media query.

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -12,19 +12,23 @@ const ExpensesFilter = (props) => {
 
   return (
     <div className="expenses-filter">
-      <div className="expenses-filter__control">
-        <label>Filter by year</label>
-        <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-        </select>
-        <label>Currency</label>
-        <select onChange={currencyChangeHandler}>
-          <option value="$">$</option>
-          <option value="Rs.">Rs.</option>
-        </select>
+      <div className="expenses-filter__container">
+        <div className="expenses-filter__control">
+          <label>Filter by year</label>
+          <select value={props.selected} onChange={dropdownChangeHandler}>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+          </select>
+        </div>
+        <div className="expenses-filter__control">
+          <label>Currency</label>
+          <select onChange={currencyChangeHandler}>
+            <option value="$">$</option>
+            <option value="Rs.">Rs.</option>
+          </select>
+        </div>
       </div>
     </div>
   );
